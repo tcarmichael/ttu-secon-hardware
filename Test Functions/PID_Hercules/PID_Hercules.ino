65607ac4cb416e2374504f3a4c0f4fc928917f4e
Tandy Carmichael <tcarmichael@frontier.com>
2014-10-09 18:43:26 -0500

    Tuned PID line-following function
    The robot now follows the line until the end. The K_d value may need to
    be increased.

--- Test Functions/PID_Hercules/PID_Hercules.ino
+++ Test Functions/PID_Hercules/PID_Hercules.ino
@@ -5,15 +5,14 @@
 
 #include <QTRSensors.h>
 
-#define Kp 1 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 0 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
-#define rightMaxSpeed 75 // max speed of the robot
-#define leftMaxSpeed 75 // max speed of the robot
-#define rightBaseSpeed 50 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define leftBaseSpeed 50  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define Kp 0.01   // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kd 0.1 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define rightMaxSpeed 40 // max speed of the robot
+#define leftMaxSpeed 40 // max speed of the robot
+#define rightBaseSpeed 15 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define leftBaseSpeed 15  // this is the speed at which the motors should spin when the robot is perfectly on the line
 #define NUM_SENSORS  5     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 us for sensor outputs to go low
-#define EMITTER_PIN   2     // emitter is controlled by digital pin 2
 
 QTRSensorsRC qtrrc((unsigned char[]) {
   12, 11, 13, 3, 2
@@ -23,14 +22,17 @@ unsigned int sensorValues[NUM_SENSORS];
 
 void setup()
 {
+  Serial.begin(9600);
+  Serial.println("Basic Line Following");
+  
   delay(500);
+  Serial.println("Begin Calibration");
   for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
   {
     qtrrc.calibrate();       // reads all sensors 10 times at 2500 us per read (i.e. ~25 ms per call)
   }
 
   // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
   for (int i = 0; i < NUM_SENSORS; i++)
   {
     Serial.print(qtrrc.calibratedMinimumOn[i]);
@@ -76,8 +78,11 @@ void loop()
 {
   unsigned int sensors[NUM_SENSORS];
   int position = qtrrc.readLine(sensors, QTR_EMITTERS_ON, true); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
-  int error = position - 2500;
-  Serial.println(position + " " + error);
+  int error = position - 2000;
+  
+  Serial.print(error);
+  Serial.print('\t');
+  Serial.println(position);
 
   int motorSpeed = Kp * error + Kd * (error - lastError);
   lastError = error;
@@ -85,13 +90,23 @@ void loop()
   int rightMotorSpeed = rightBaseSpeed + motorSpeed;
   int leftMotorSpeed = leftBaseSpeed - motorSpeed;
 
-  if (rightMotorSpeed > rightMaxSpeed ) rightMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
-  if (leftMotorSpeed > leftMaxSpeed ) leftMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
-  if (rightMotorSpeed < 0) rightMotorSpeed = 0; // keep the motor speed positive
-  if (leftMotorSpeed < 0) leftMotorSpeed = 0; // keep the motor speed positive
+  // Switch the motor direction for negatives
+  unsigned char rightDirection = DIRF;
+  unsigned char leftDirection = DIRR;
+  if (rightMotorSpeed < 0) {
+   rightMotorSpeed = -rightMotorSpeed;
+   rightDirection = DIRR;
+  }
+  if (leftMotorSpeed < 0) {
+    leftMotorSpeed = -leftMotorSpeed;
+    leftDirection = DIRF;
+  }
+
+  if (rightMotorSpeed > rightMaxSpeed) rightMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
+  if (leftMotorSpeed > leftMaxSpeed) leftMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
 
   {
-    MOTOR.setSpeedDir1(rightMotorSpeed, DIRF);
-    MOTOR.setSpeedDir2(leftMotorSpeed, DIRR);
+    MOTOR.setSpeedDir1(leftMotorSpeed, leftDirection);
+    MOTOR.setSpeedDir2(rightMotorSpeed, rightDirection);
   }
 }
