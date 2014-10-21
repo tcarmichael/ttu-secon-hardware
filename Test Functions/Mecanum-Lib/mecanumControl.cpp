2a5e89405a089598f8968064832600d4df6d619a
datmajaya <12345galileo@gmail.com>
2014-10-21 16:29:03 -0500

    Update mecanumControl.cpp
    Changed direction of the motor from REVERSE to BACKWARD

--- Test Functions/Mecanum-Lib/mecanumControl.cpp
+++ Test Functions/Mecanum-Lib/mecanumControl.cpp
@@ -3,15 +3,13 @@
 #include "mecanumControl.h"
 
 mecanumControl::mecanumControl(){
-  for(int i=1; i<=4; i++){
-    point_moto[i-1] = Moto_Shield.getMotor(i);
-  }
+	Moto_Shield.begin();
 }
 
 // input magnitude has to be between -1 and 1
 // input angle has to be between 0 and 2 PI
 // input rotation has to be between -1 and 1
-void mecanumControl::mecanumControl(float magnitude, float angle, float rotation) {
+void mecanumControl::mecRun(float magnitude, float angle, float rotation) {
   
   // Wheel 1 is front left, wheel 2 is front right
   // Wheel 3 is rear left, wheel4 is rear right
@@ -44,26 +42,67 @@ void mecanumControl::mecanumControl(float magnitude, float angle, float rotation
   }
 */
   // Normalizing the voltage multiplier for the motor outputs
-  float VN[4];
+  float VN1, VN2, VN3, VN4;
   
-  VN[0] = V1 * (255 / 2.0f);
-  VN[1] = V2 * (255 / 2.0f);
-  VN[2] = V3 * (255 / 2.0f);
-  VN[3] = V4 * (255 / 2.0f); 
+  VN1 = V1 * (255 / 2.0f);
+  VN2 = V2 * (255 / 2.0f);
+  VN3 = V3 * (255 / 2.0f);
+  VN4 = V4 * (255 / 2.0f); 
     
   // send the voltages to the motor driver
-  for(int i=0; i<4; i++){
-    if(VN[i] = 0){
-	  //point_moto[i]->setSpeed(abs(VN[i]));
-      point_moto[i]->run(RELEASE);
-    }
-    else if(VN[i] > 0){
-	  point_moto[i]->setSpeed(abs(VN[i]));
-      point_moto[i]->run(FORWARD);
-    }
-    else{
-	  point_moto[i]->setSpeed(abs(VN[i]));
-      point_moto[i]->run(REVERSE);
-    }
+  // to motor1
+  if(VN1 = 0){
+	//motor1->setSpeed(abs(VN1));
+    motor1->run(RELEASE);
+  }
+  else if(VN1 > 0){
+	motor1->setSpeed(abs(VN1));
+    motor1->run(FORWARD);
+  }
+  else{
+    motor1->setSpeed(abs(VN1));
+    motor1->run(BACKWARD);
+  }
+  
+  // to motor2
+  if(VN2 = 0){
+	//motor2->setSpeed(abs(VN2));
+    motor2->run(RELEASE);
+  }
+  else if(VN2 > 0){
+	motor2->setSpeed(abs(VN2));
+    motor2->run(FORWARD);
+  }
+  else{
+    motor2->setSpeed(abs(VN2));
+    motor2->run(BACKWARD);
+  }
+	
+  // to motor3
+  if(VN3 = 0){
+	//motor3->setSpeed(abs(VN3));
+    motor3->run(RELEASE);
+  }
+  else if(VN3 > 0){
+	motor3->setSpeed(abs(VN3));
+    motor3->run(FORWARD);
+  }
+  else{
+    motor3->setSpeed(abs(VN3));
+    motor3->run(BACKWARD);
+  }
+  
+  // to motor4
+  if(VN4 = 0){
+	//motor4->setSpeed(abs(VN4));
+    motor4->run(RELEASE);
+  }
+  else if(VN4 > 0){
+	motor4->setSpeed(abs(VN4));
+    motor4->run(FORWARD);
+  }
+  else{
+    motor4->setSpeed(abs(VN4));
+    motor4->run(BACKWARD);
   }
 }
