657d871c71d44cae8d238b574200f1766c385196
datmajaya <12345galileo@gmail.com>
2014-10-21 16:27:23 -0500

    Update mecanumControl.h
    Changed the name of the constructor and changed the pointers to the motors.

--- Test Functions/Mecanum-Lib/mecanumControl.h
+++ Test Functions/Mecanum-Lib/mecanumControl.h
@@ -1,19 +1,23 @@
 #ifndef mecanumControl_h
-#define mecanumControl_h 
-//#define PI 3.14159265
-#define PI_4 0.78539816
-#include "Arduino.h"
+#define mecanumControl_h
+
 #include <Wire.h>
 #include "../Adafruit_MotorShield/Adafruit_MotorShield.h"
 //#include "utility/Adafruit_PWMServoDriver.h"
+
+//#define PI 3.14159265
+#define PI_4 0.78539816
 
 class mecanumControl
 {
 	public:
 		mecanumControl();
-		void mecanumControl(float magnitude, float angle, float rotation); 
+		void mecRun(float magnitude, float angle, float rotation); 
 	private:
 		Adafruit_MotorShield Moto_Shield = Adafruit_MotorShield();
-		Adafruit_DCMotor *point_moto[] = {0,0,0,0};
-}
+		Adafruit_DCMotor *motor1 = Moto_Shield.getMotor(1);
+		Adafruit_DCMotor *motor2 = Moto_Shield.getMotor(2);
+		Adafruit_DCMotor *motor3 = Moto_Shield.getMotor(3);
+		Adafruit_DCMotor *motor4 = Moto_Shield.getMotor(4);
+};
 #endif
