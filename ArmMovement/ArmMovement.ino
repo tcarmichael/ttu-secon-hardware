a7542059978c99e61809a8b00d09ff7efd2cb314
jrjohnsonttu <jrjohnsonttu@gmail.com>
2014-11-16 22:13:38 -0600

    Tada

--- ArmMovement/ArmMovement.ino
+++ ArmMovement/ArmMovement.ino
@@ -28,23 +28,5 @@ void loop()
 	Arm_Front_Flip_To_Right();
 	Arm_Front_Home_Right();*/
 
-	control.rearArm(-8, 2, 0, 90, 90, -90);
-	delay(1500);
-
-	control.rearArm(-8.5, 2, -3, 90, 90, -90);
-	delay(1500);
-
-	for (double i = 0; i < 4; i = +.1) {
-		control.rearArm(-8.5+i, 2, -3, 90, 90, -90);
-		delay(100);
-	}
+	
 }
-/* I'm not sure how these functions are different from the other functions
-void Arm_Front_Home()
-{
-}
-
-void Arm_Rear_Home()
-{
-	Rear_Arm_IK(3, 0, 3, 90, 90, -90);
-}*/
