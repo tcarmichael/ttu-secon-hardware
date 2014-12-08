aa5f7d88eec3562148a77d5bae5d41a522be5ba3
jrjohnsonttu <jrjohnsonttu@gmail.com>
2014-12-08 17:32:19 -0600

    Update RubiksControl.cpp

--- LineFollowingAllBranches/RubiksControl.cpp
+++ LineFollowingAllBranches/RubiksControl.cpp
@@ -4,90 +4,207 @@ double gripAngle = 115;
 void RubiksControl::Rubiks_Play()
 {
 	Grab();
-	//Rotate();
-	//Release();
+	Rotate();
+	Release();
 }
 void RubiksControl::Grab()
 {
+	double TimeConstant=.6;
 	
-	//grab hat from skid
+	
 	parent->frontHomeLeft();
 	parent->RearHomeLeft();
-	parent->Front_Smooth_Move(-1, 0, 2, 90, 90, -30,    -1, 0, 2, 90, 90, 90,    1); //move front arm out of the way
-
-	parent->Rear_Smooth_Move(1, 0, 2, 90, 90, -30,     3.9, 2.2, -0.1, 20, 120, -105,   1); //move rear arm to grab rubik hat - above it
-	delay(1500);
-
-	parent->Rear_Smooth_Move(3.9, 2.2, -0.1, 20, 120, -105,      4.9, 2.8, -2.8, 0, 120, -105,     1); //move rear arm to grab rubik hat -bend to it
-	delay(1500);
 
 
-	parent->Rear_Smooth_Move(4.9, 2.8, -2.8, 0, 120, -105,      4.9, 2.8, -3.4, 0, 120, -105,      1); //move rear arm to grab rubik hat -grasp it
-	delay(1500);
 
-	parent->Rear_Smooth_Move(4.9, 2.8, -2.8, 0, 120, -105,       4.9, 2.8, -3.4, 90, 120, -105,      1); //squeeze
-	delay(1500);
+	//grab hat from skid
 
-	parent->Rear_Smooth_Move(4.9, 2.8, -3.4, 90, 120, -105,      4.9, 2.8, 0, 90, 120, -105,      1); //move rear arm up out of the way
-	delay(1500);
 
-	parent->Rear_Smooth_Move(4.9, 2.8, 0, 90, 120, -105,         8, 0, 0, 90, 120, -105, 1); //move rear arm up out 
-	delay(1500);
+	
+	double A001[6]={1, 0, 2, 90, 90, -30};
+	double C001[6]={ 3.9, 2.2, -0.1, 20, 120, -105};
+	double M001[6]={-1, 0, 2, 90, 90, -30};
+	double N001[6]={  -1, 0, 2, 90, 90, 90};
 
-	parent->Rear_Smooth_Move(8, 0, 0, 90, 120, -105,             8, 0, -5, 90, 120, -105, 1); //move rear arm down 
-	delay(1500);
 
-	//grab rubik
-	/*parent->Front_Smooth_Move(-1, 0, 2, 85, 90, 90, -8.5, 1.5, 0, 90, 90, -50, 1); //extend front arm out to rubik
-	parent->Front_Smooth_Move(-8.5, 1.5, 0, 90, 90, -50, -8.5, 1.4, -3.5, 90, 65, -90, 1);//low arm to grab rubik
-	delay(500);
-	parent->Front_Smooth_Move(-8.5, 1.4, -3.5, 90, 65, -90, -6.6, 1.4, -3, 90, 65, -90, 1); //bring rubik into skid
-	delay(500);
-	parent->Front_Smooth_Move(-6.6, 1.5, -3, 90, 65, -90, -6.6, 1.5, 0, 90, 65, -90, 1); // rise above cube
-	delay(500);
-	parent->Front_Smooth_Move(-6.6, 1.5, 0, 90, 65, -90, -1, 0, 2, 90, 90, 90, 1); //return to home and out of the way
+	parent->Both_Smooth_Move(A001,C001,M001,N001,TimeConstant);
+
+
+	/*parent->Front_Smooth_Move(-1, 0, 2, 90, 90, -30,    -1, 0, 2, 90, 90, 90,    TimeConstant); //move front arm out of the way
+	delay(100);
+	
+	parent->Rear_Smooth_Move(1, 0, 2, 90, 90, -30,     3.9, 2.2, -0.1, 20, 120, -105,   TimeConstant); //move rear arm to grab rubik hat - above it
+	delay(100);
 	*/
+	parent->Rear_Smooth_Move(3.9, 2.2, -0.1, 20, 120, -105,      4.0, 2.5, -3.1, 0, 120, -105,    TimeConstant); //move rear arm to grab rubik hat -bend to it
+	delay(100);
+
+	parent->Rear_Smooth_Move(4.0, 2.5, -3.1, 0, 120, -105,      4.0, 2.5, -3.1, 110, 120, -105,      TimeConstant); //move rear arm to grab rubik hat -grasp it
+	delay(100);
+
+	parent->Rear_Smooth_Move(4.1, 2.5, -3.1, 110, 120, -105,       4.1, 2.5, 0, 110, 120, -105,      TimeConstant); //squeeze
+	delay(100);
+
+
+
+
+	// Forcing Rubiks to center position
+
+
+	// move both arms out
+	double A011[6]={4.1, 2.5, 0, 110, 120, -105};
+	double C011[6]={  8, 0, 0, 110, 90, -105};
+	double M011[6]={-1, 0, 2, 90, 90, 90};
+	double N011[6]={ -9, 0, 0, 0, 163, -105};
+
+
+	parent->Both_Smooth_Move(A011,C011,M011,N011,TimeConstant);
+
+
+	//parent->Rear_Smooth_Move(4.1, 2.5, 0, 110, 120, -105,         8, 0, 0, 110, 90, -105, TimeConstant); //move rear arm up out 
+	delay(100);
+
+	
+// move both arms down
+	double A01[6]={8, 0, 0, 110, 90, -105};
+	double C01[6]={ 8, 0, -4, 110, 90, -105};
+	double M01[6]={-9, 0, 0, 0, 163, -105};
+	double N01[6]={ -9, 0, -4.3, 0, 163, -105};
+
+
+	parent->Both_Smooth_Move(A01,C01,M01,N01,TimeConstant);
+
+
+// Squeeze together
+	double A[6]={8.5, 0, -4, 110, 92, -105};
+	double B[6]={8.5, 1.6, -4.3, 110, 92, -105};
+	double M[6]={-8.5, 0, -4.3, 0, 163, -105};
+	double N[6]={-8.5, .7, -4.3, 0, 163, -105};
+
+
+	parent->Both_Smooth_Move(A,B,M,N,TimeConstant);
+
+	
+	// Pull out
+	double A2[6]={8.5, 1.7, -4.3, 110, 92, -105};
+	double B2[6]={8.5, 0, -4.3, 110, 92, -105};
+	double M2[6]={-8.5,.6, -4.3, 0, 163, -105};
+	double N2[6]={-9, 0, -4.3, 0, 163, -105};
+
+
+	parent->Both_Smooth_Move(A2,B2,M2,N2,TimeConstant);
+
+	// Pull in setup
+	double A3[6]={8.5, 0, -4.3, 110, 92, -105};
+	double B3[6]={6, 0, 2, 110, 92, -105};
+	double M3[6]={-9, 0, -4.3, 0, 163, -105};
+	double N3[6]={-9, 0, -3, 0, 163, 0};
+
+
+	parent->Both_Smooth_Move(A3,B3,M3,N3,TimeConstant);
+
+	// Pull in setup step 2
+	parent->Front_Smooth_Move(-9, 0, -3, 0, 163, 0,           -9, 2, -5, 0, 80, 0, TimeConstant);
+	delay(100);
+
+	// pull in wrist swing down
+	parent->Front_Smooth_Move(-9, 2, -5, 0, 80, 0,           -9, 2, -5, 0, 65, -90, TimeConstant);
+	delay(100);
+
+	// pull in x direction 
+	parent->Front_Smooth_Move(-9, 2, -5, 0, 65, -90,           -5, 2, -5, 0, 65, -90, TimeConstant);
+	delay(100);
+
+	// move out the way
+	parent->Front_Smooth_Move(-7, 2, -5, 0, 65, -90,           -8, 0, -5, 0, 65, 90, TimeConstant);
+	delay(100);
+
+	// Place hat position
+	double A4[6]={6, 0, 2, 110, 92, -105};
+	double B4[6]={4.7, 2.9, -2, 110, 33, -105};
+	double M4[6]={ -8, 0, -5, 0, 65, 90};
+	double N4[6]={-1, 0, 2, 90, 90, 90};
+
+
+	parent->Both_Smooth_Move(A4,B4,M4,N4,TimeConstant);
+	
+	
+
+	
+	
 }
 
 
 void RubiksControl::Rotate()
 {
-	//parent->frontHomeLeft(); //testing only
-	parent->Rear_Smooth_Move(1, 0, 2, gripAngle, 0, 90, 4, 0, 2, gripAngle, 90, -90, 1); //move rear arm 
-	delay(1500);
-	parent->Rear_Smooth_Move(4, 0, 2, gripAngle, 90, -90, 5.2, 2.5, 0, gripAngle, 30, -100, 1); //move rear arm over rubik
-	delay(1500);
-	parent->Rear_Smooth_Move(5.2, 2.5, 0, gripAngle, 30, -100, 4.5, 2.5, -.8, gripAngle, 30, -100, 1); //bring hat onto rubik
-	delay(1500);
+	double TimeConstant=.4;
+	// place hat on it
+	parent->Rear_Smooth_Move(4.7, 2.9, -2, 110, 33, -105,                        4.7, 2.5, -2.7, 110, 33, -105,                      TimeConstant); 
+	delay(100);
+
 	// Fudge factoring
-	parent->Rear_Smooth_Move(4.5, 2.5, -.8, gripAngle, 30, -100, 4.5, 2.3, -.8, gripAngle, 30, -100, 1); //really make sure hat is there
-	delay(1500);
+	parent->Rear_Smooth_Move( 4.7, 3.1, -2.5, 110, 33, -105,                          4.7, 2.3, -2.3, 110, 33, -105,                        TimeConstant); //really make sure hat is there
+	delay(100);
+	parent->Rear_Smooth_Move( 4.5, 2.5, -2.5, 110, 33, -105,                          4.9, 2.5, -2.5, 110, 33, -105,                        TimeConstant); //really make sure hat is there
+	delay(100);
 
+	parent->Rear_Smooth_Move(4.7, 2.9, -2.5, 110, 33, -105,                        4.3, 2.9, -3, 110, 33, -105,                      TimeConstant); //Push hat down and back
+	delay(100);
+	
 
-	parent->Rear_Smooth_Move(4.8, 2.5, -.8, gripAngle, 30, -100, 4.2, 2.5, -.8, gripAngle, 136, -100, 1); //rotate hat
-	delay(1500);
+	//// rotating
+	parent->Rear_Smooth_Move(  4.5, 2.9, -3, 110, 33, -105,                     4.8, 2.9, -2.7, 110, 180, -105,  TimeConstant);                               //rotate hat
+	delay(100);
 
 	//reset hat
-	parent->Rear_Smooth_Move(4.2, 2.5, -.8, gripAngle, 140, -100, 4.2, 2.5, 0, gripAngle, 140, -100, 1); //lift hat up
-	parent->Rear_Smooth_Move(4.2, 2.5, 0, gripAngle, 140, -100, 5.2, 2.5, 0, gripAngle, 30, -100, 1); //reset hat
-	parent->Rear_Smooth_Move(5.2, 2.5, 0, gripAngle, 30, -100, 4.5, 2.5, -.8, gripAngle, 30, -100, 1); //bring hat onto rubik
-	delay(1500);
-	// Fudge factoring
-	parent->Rear_Smooth_Move(4.5, 2.5, -.8, gripAngle, 30, -100, 4.5, 2.3, -.8, gripAngle, 30, -100, 1); //really make sure hat is there
-	delay(1500);
-	parent->Rear_Smooth_Move(4.8, 2.5, -.8, gripAngle, 30, -100, 4.2, 2.5, -.8, gripAngle, 136, -100, 1); //rotate hat
-	delay(1500);
+	parent->Rear_Smooth_Move(4.5, 2.9, -2.5, 110, 180, -105,                  4.5, 2.9, -2.5, 0, 180, -105,           TimeConstant); //open grip
+	delay(100);
+	parent->Rear_Smooth_Move(4.5, 2.9, -2.5, 0, 180, -105,                     4.5, 2.9, -2, 0, 180, -105,           TimeConstant); //move up
+	delay(100);
+	parent->Rear_Smooth_Move(4.5, 2.9, -2, 0, 180, -105,                        4.5, 2.9, -2, 0, 0, -105,           TimeConstant); //reset wrist
+	delay(100);
+	parent->Rear_Smooth_Move(  4.5, 2.9, -2, 0, 0, -105,                4.5, 2.9, -3, 0, 0, -105,           TimeConstant); //lower
+	delay(100);
+	parent->Rear_Smooth_Move(   4.5, 2.9, -3, 0, 0, -105,                4.5, 2.9, -3, 110, 0, -105,           TimeConstant); //squeeze
+	delay(100);
+	parent->Rear_Smooth_Move(    4.5, 2.9, -3, 110, 0, -105,               4.3, 2.9, -3, 110, 0, -105,           TimeConstant); // pull back just in case
+	delay(100);
+
+
+	// rotate again
+	parent->Rear_Smooth_Move(  4.6, 2.9, -3, 110, 0, -105,               4.6, 2.9, -3, 105, 70, -105,           TimeConstant); //rotate
+	delay(100);
 
 
 }
 
 void RubiksControl::Release()
 {
-	parent->Rear_Smooth_Move(4.2, 2.5, -.8, gripAngle, 140, -100, 4.2, 2.5, 0, gripAngle, 140, -100, 1); //lift hat up
-	parent->Rear_Smooth_Move(4.2, 2.5, 0, gripAngle, 140, -100, 1, 0, 2, gripAngle, 0, -30, 1); //return to home
-	//parent->frontHomeLeft();
-	//parent->FrontFlipToRight();
-	//parent->frontHomeRight();
+	double TimeConstant=.4;
+	// ditch hat and cube
+	parent->Rear_Smooth_Move(  4.6, 2.9, -3, 105, 70, -105,                  8.5, 2.9, -3, 105, 70, -105,           TimeConstant); //pull out
+	delay(100);
+
+	parent->Rear_Smooth_Move(   8.5, 2.9, -3, 105, 70, -105,                  8.5, 2.9, -3, 105, 110, -105,           TimeConstant); //rotate cube
+	delay(100);
+
+	parent->Rear_Smooth_Move(     8.5, 2.9, -3, 105, 110, -105,                   8.5, 2.9, 1, 105, 110, -105,           TimeConstant); //pull up
+	delay(100);
+
+	parent->Rear_Smooth_Move(   9, 2.9, 2, 110, 70, -105,                   4.0, 2.5, -2, 110, 120, -105,      TimeConstant); //place back hat
+	delay(1000);
+
+	parent->Rear_Smooth_Move(    4.0, 2.5, -2, 110, 120, -105,             4.4, 2.5, -3.1, 110, 120, -105,       TimeConstant); //drop hat down
+	delay(1000);
+
+	parent->Rear_Smooth_Move(     4.4, 2.5, -3.1, 110, 120, -105,         4.4, 2.5, -3.1, 0, 120, -105,       TimeConstant); // hat leggo
+	delay(1000);
+
+	parent->Rear_Smooth_Move(    4.4, 2.5, -3.1, 110, 120, -105,             4.2, 2.5, 2, 0, 120, -105,       TimeConstant); //move up
+	delay(100);
+
+	parent->RearHomeLeft();
+	parent->frontHomeLeft();
 	
 }
 
