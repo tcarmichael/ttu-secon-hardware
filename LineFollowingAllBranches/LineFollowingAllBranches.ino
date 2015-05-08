6653989195260e7d23cc5e1b8cbe1609f2e62bd8
smnolen42 <smnolen42@students.tntech.edu>
2015-05-08 12:50:35 -0500

    added etch functionality

--- LineFollowingAllBranches/LineFollowingAllBranches.ino
+++ LineFollowingAllBranches/LineFollowingAllBranches.ino
@@ -8,11 +8,13 @@
 #include "Adafruit_Sensor\Adafruit_Sensor.h"
 #include "Robot.h"
 #include "ArduinoJson.h"
+#include <string>
+#include <stdio.h>
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
+#include "Arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 // Interacts with the mecanum wheels and the motor shield
@@ -36,12 +38,12 @@ void setup() {
 
 	//Example code Line for LEDs, just change the color word
 	leds.LED_Setup();
-	
+
 	// Initialize the mecanum wheels
 	Serial.println("Initializing the mecanum wheels");
 	mecanum.begin();
 	mecanum.mecRun(0, 0, 0);
-	
+
 	// Calibrate the line followers
 	Serial.println("Calibrating");
 	//lineFollowerControl.defaultCalibration();
@@ -49,19 +51,19 @@ void setup() {
 
 	// Set the arms in position
 	arm.begin();
-	arm.frontHomeLeft();
-	arm.RearHomeLeft();
-	//arm.frontHomeRight();
-	//arm.RearHomeRight();
-	
+	//arm.frontHomeLeft();
+	//arm.RearHomeLeft();
+	arm.frontHomeRight();
+	arm.RearHomeRight();
+
 
 	/*****Robot is initialized and ready to run at this point*****/
 
 
 
 
-	
-	
+
+
 }
 
 void loop() {
@@ -70,7 +72,7 @@ void loop() {
 	//{"id":"etch","etchname":"Savannah"}
 	while (Serial.available() > 0) {
 		char c = Serial.read();
-		
+
 		if (readBufferIndex < maxReadBuffer - 1) {
 			readBuffer[readBufferIndex++] = c;
 		}
@@ -118,27 +120,216 @@ void loop() {
 					{
 						const char *name = root["etchname"];
 						int count = 0;
+						int DownUp = 1; //1 means call a down, 0 means calls an up letter
 						int number_of_letters = strlen(name);
 						Serial.println(number_of_letters);
 						Serial.println(name);
-							while (count < number_of_letters)
+						arm.Etch.Grasp();
+						while (count < number_of_letters && count < 8)
 						{
-							if (count % 2 == 0)
+							//char token = *name[count];
+							if (DownUp == 1)
 							{
-								//do even letter functions
-								Serial.println(name[count]);
-								Serial.println("up");
-
-							}
-							if (count % 2 == 1)
-							{
-								//do down letters
+								//do down letter functions
 								Serial.println(name[count]);
 								Serial.println("down");
+								if (toupper(name[count]) == 'A')
+								{
+									DownUp = arm.Etch.letterAdown();
+								}
+								if (toupper(name[count]) == 'B')
+								{
+									DownUp = arm.Etch.letterBdown();
+								}
+								if (toupper(name[count]) == 'C')
+								{
+									DownUp = arm.Etch.letterCdown();
+								}
+								if (toupper(name[count]) == 'D')
+								{
+									DownUp = arm.Etch.letterDdown();
+								}
+								if (toupper(name[count]) == 'E')
+								{
+									DownUp = arm.Etch.letterEdown();
+								}
+								if (toupper(name[count]) == 'F')
+								{
+									DownUp = arm.Etch.letterFdown();
+								}
+								if (toupper(name[count]) == 'G')
+								{
+									DownUp = arm.Etch.letterGdown();
+								}
+								if (toupper(name[count]) == 'H')
+								{
+									DownUp = arm.Etch.letterHdown();
+								}
+								if (toupper(name[count]) == 'I')
+								{
+									DownUp = arm.Etch.letterIdown();
+								}
+								if (toupper(name[count]) == 'J')
+								{
+									DownUp = arm.Etch.letterJdown();
+								}
+								if (toupper(name[count]) == 'K')
+								{
+									DownUp = arm.Etch.letterKdown();
+								}
+								if (toupper(name[count]) == 'L')
+								{
+									DownUp = arm.Etch.letterLdown();
+								}
+								if (toupper(name[count]) == 'M')
+								{
+									DownUp = arm.Etch.letterMdown();
+								}
+								if (toupper(name[count]) == 'N')
+								{
+									DownUp = arm.Etch.letterNdown();
+								}
+								if (toupper(name[count]) == 'O')
+								{
+									DownUp = arm.Etch.letterOdown();
+								}
+								if (toupper(name[count]) == 'P')
+								{
+									DownUp = arm.Etch.letterPdown();
+								}
+								if (toupper(name[count]) == 'Q')
+								{
+									DownUp = arm.Etch.letterQdown();
+								}
+								if (toupper(name[count]) == 'R')
+								{
+									DownUp = arm.Etch.letterRdown();
+								}
+								if (toupper(name[count]) == 'S')
+								{
+									DownUp = arm.Etch.letterSdown();
+								}
+								if (toupper(name[count]) == 'T')
+								{
+									DownUp = arm.Etch.letterTdown();
+								}
+								if (toupper(name[count]) == 'U')
+								{
+									DownUp = arm.Etch.letterUdown();
+								}
+								if (toupper(name[count]) == 'V')
+								{
+									DownUp = arm.Etch.letterVdown();
+								}
+								if (toupper(name[count]) == 'W')
+								{
+									DownUp = arm.Etch.letterWdown();
+								}
+
+
+							}
+							else
+							{
+								//do up letters
+								Serial.println(name[count]);
+								Serial.println("up");
+								if (toupper(name[count]) == 'A')
+								{
+									DownUp = arm.Etch.letterAup();
+								}
+								if (toupper(name[count]) == 'B')
+								{
+									DownUp = arm.Etch.letterBup();
+								}
+								if (toupper(name[count]) == 'C')
+								{
+									DownUp = arm.Etch.letterCup();
+								}
+								if (toupper(name[count]) == 'D')
+								{
+									DownUp = arm.Etch.letterDup();
+								}
+								if (toupper(name[count]) == 'E')
+								{
+									DownUp = arm.Etch.letterEup();
+								}
+								if (toupper(name[count]) == 'F')
+								{
+									DownUp = arm.Etch.letterFup();
+								}
+								if (toupper(name[count]) == 'G')
+								{
+									DownUp = arm.Etch.letterGup();
+								}
+								if (toupper(name[count]) == 'H')
+								{
+									DownUp = arm.Etch.letterHup();
+								}
+								if (toupper(name[count]) == 'I')
+								{
+									DownUp = arm.Etch.letterIup();
+								}
+								if (toupper(name[count]) == 'J')
+								{
+									DownUp = arm.Etch.letterJup();
+								}
+								if (toupper(name[count]) == 'K')
+								{
+									DownUp = arm.Etch.letterKup();
+								}
+								if (toupper(name[count]) == 'L')
+								{
+									DownUp = arm.Etch.letterLup();
+								}
+								if (toupper(name[count]) == 'M')
+								{
+									DownUp = arm.Etch.letterMup();
+								}
+								if (toupper(name[count]) == 'N')
+								{
+									DownUp = arm.Etch.letterNup();
+								}
+								if (toupper(name[count]) == 'O')
+								{
+									DownUp = arm.Etch.letterOup();
+								}
+								if (toupper(name[count]) == 'P')
+								{
+									DownUp = arm.Etch.letterPup();
+								}
+								if (toupper(name[count]) == 'Q')
+								{
+									DownUp = arm.Etch.letterQup();
+								}
+								if (toupper(name[count]) == 'R')
+								{
+									DownUp = arm.Etch.letterRup();
+								}
+								if (toupper(name[count]) == 'S')
+								{
+									DownUp = arm.Etch.letterSup();
+								}
+								if (toupper(name[count]) == 'T')
+								{
+									DownUp = arm.Etch.letterTup();
+								}
+								if (toupper(name[count]) == 'U')
+								{
+									DownUp = arm.Etch.letterUup();
+								}
+								if (toupper(name[count]) == 'V')
+								{
+									DownUp = arm.Etch.letterVup();
+								}
+								if (toupper(name[count]) == 'W')
+								{
+									DownUp = arm.Etch.letterWup();
+								}
 							}
 							count++;
+							arm.Etch.makeSpace();
 						}
-
+						arm.Etch.ReleaseDemo();
 
 					}
 
@@ -152,7 +343,6 @@ void loop() {
 		}
 	}
 }
-
 
 
 
